1976f6d0d66f5506d774eb02ca11e0551e257d3b
MartinPolicansky <martin@dreamy.cz>
2025-08-30 01:57:02 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,2 +1,5 @@
-export { auth as middleware } from 'next-auth/middleware'
-export const config = { matcher: ['/dashboard/:path*', '/api/(months|capex|cashflow|kpi):path*'] }
+export { auth as middleware } from "@/lib/auth";
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/api/:path*"]
+};
